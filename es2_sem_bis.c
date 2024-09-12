b30f60bad9d9c6e66cf9bb956dbd9edd34dd5d6d
Ilaria00 <82587000+Ilaria00@users.noreply.github.com>
2024-09-12 18:28:23 +0200

    Update es2_sem_bis.c

--- es2_sem_bis.c
+++ es2_sem_bis.c
@@ -1,3 +1,4 @@
+
 #include <time.h>
 #include <stdio.h>
 #include <semaphore.h>
@@ -6,166 +7,183 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+
+#define my_printf(x, ...) printf(x, ##__VA_ARGS__); fflush(stdout)
 #define do_something() usleep(rand() % 1000 + 1000)
 
-struct gestore_t {
+
+struct handler_t {
     sem_t mutex;
+
     sem_t reset_s;
     sem_t procAoB_s;
     
     int reset_attesa, AoB_attesa;
     int reset_esecuzione, AoB_esecuzione;
-} gestore;
+} g_handler;
 
-pthread_mutex_t id_mutex = PTHREAD_MUTEX_INITIALIZER;
-int id_A = 0;
-int id_B = 0;
-int id_R = 0;
 
-void gestore_init (struct gestore_t *g) {
+void handler_init(struct handler_t* handler)
+{
     sem_init(&g->mutex, 0, 1);
+
     sem_init(&g->reset_s, 0, 0);
     sem_init(&g->procAoB_s, 0, 0);
+
     g->reset_attesa = g->AoB_attesa = 0;
     g->reset_esecuzione = g->AoB_esecuzione = 0;
 }
 
-void StartProcAorProcB (struct gestore_t *g){
-    sem_wait(&g->mutex);
-    if (g->reset_esecuzione || g->AoB_esecuzione) {
-        g->AoB_attesa++;
-        sem_post(&g->mutex);
-        sem_wait(&g->procAoB_s);
-        g->AoB_attesa--;
-    } else {
-        g->AoB_esecuzione++;
-        sem_post(&g->mutex);
+void handler_print(struct handler_t* handler)
+{
+    my_printf("cab=%d, cr=%d, nab=%d, nr=%d\n", handler->cab, handler->cr, handler->nab, handler->nr);
+}
+
+// ------------------------------------------------------------------------------------------------
+
+void StartProcAOrB()
+{
+    sem_wait(&g_handler.mutex);
+
+    //se ci sono processi reset o AoB in esecuzione mi blocco
+    if (g_handler.reset_esecuzione || g_handler.AoB_esecuzione) {
+        g_handler.AoB_attesa++;
+        sem_post(&g_handler.mutex);
+        sem_wait(&g_handler.procAoB_s);
+        g_handler.AoB_attesa--;
+    }
+    else {
+        g_handler.AoB_esecuzione++;
+    }
+    sem_post(&g_handler.mutex);
+}
+
+void EndProcAOrB()
+{
+    sem_wait(&g_handler.mutex);
+    g_handler.AoB_esecuzione--;
+    //se ci sono processi reset in attesa li sveglio
+    if (g_handler.reset_attesa) {
+        g_handler.reset_esecuzione++; 
+        sem_post(&g_handler.reset_s);
+    }
+    else if (g_handler.AoB_attesa) {
+        g_handler.AoB_esecuzione++; 
+        sem_post(&g_handler.procAoB_s);
+    }
+    else {
+        sem_post(&g_handler.mutex);
     }
 }
 
-void EndProcAorProcB (struct gestore_t *g) {
-    sem_wait(&g->mutex);
-    g->AoB_esecuzione--;
-    if (g->reset_attesa) {
-        g->reset_esecuzione++; 
-        sem_post(&g->reset_s);
-    } else if (g->AoB_attesa) {
-        g->AoB_esecuzione++; 
-        sem_post(&g->procAoB_s);
-    } else {
-        sem_post(&g->mutex);
+void StartReset()
+{
+    sem_wait(&g_handler.mutex);
+
+    //se ci sono processi AoB in esecuzione in questo momento mi blocco
+    if (g_handler.AoB_esecuzione || g_handler.reset_esecuzione) {
+        g_handler.reset_attesa++;
+        sem_post(&g_handler.mutex);
+        sem_wait(&g_handler.reset_s);
+        g_handler.reset_attesa--;
+    }
+    else {
+        g_handler.reset_esecuzione++;
+    }
+    sem_post(&g_handler.mutex);
+}
+
+void EndReset()
+{
+     sem_wait(&g_handler.mutex);
+
+    g_handler.reset_esecuzione--;
+
+    if (g_handler.reset_attesa) {
+        g_handler.reset_esecuzione++; 
+        sem_post(&g_handler.reset_s);
+    }
+    else if (g_handler.AoB_attesa) {
+        g_handler.AoB_esecuzione++; 
+        sem_post(&g_handler.procAoB_s);
+    }
+    else {
+        sem_post(&g_handler.mutex);
     }
 }
 
-void StartReset (struct gestore_t *g) {
-    sem_wait(&g->mutex);
-    if (g->AoB_esecuzione || g->reset_esecuzione) {
-        g->reset_attesa++;
-        sem_post(&g->mutex);
-        sem_wait(&g->reset_s);
-        g->reset_attesa--;
-    } else {
-        g->reset_esecuzione++;
-        sem_post(&g->mutex);
+// ------------------------------------------------------------------------------------------------
+// Main
+// ------------------------------------------------------------------------------------------------
+
+void ProcA(int thread_idx)
+{
+    StartProcAOrB();
+
+    my_printf("<%dA", thread_idx); do_something(); my_printf("A%d>", thread_idx);
+
+    EndProcAOrB();
+}
+
+void ProcB(int thread_idx)
+{
+    StartProcAOrB();
+
+    my_printf("<%dB", thread_idx); do_something(); my_printf("B%d>", thread_idx);
+
+    EndProcAOrB();
+}
+
+void Reset(int thread_idx)
+{
+    StartReset();
+
+    my_printf("<%dR", thread_idx); do_something(); my_printf("R%d>", thread_idx);
+
+    EndReset();
+}
+
+void* thread_body(void* arg)
+{
+    int thread_idx = *((int*) arg);
+    while (1)
+    {
+        int r = rand();
+
+        //my_printf("|%d->%s|", thread_idx, (r%3 ==0  ? "A" : (r%3==1 ? "B" : "R")));
+
+        if (r % 3 == 0) ProcA(thread_idx);
+        else if (r % 3 == 1) ProcB(thread_idx);
+        else if (r % 3 == 2) Reset(thread_idx);
     }
 }
 
-void EndReset (struct gestore_t *g) {
-    sem_wait(&g->mutex);
-    g->reset_esecuzione--;
-    if (g->reset_attesa) {
-        g->reset_esecuzione++; 
-        sem_post(&g->reset_s);
-    } else if (g->AoB_attesa) {
-        g->AoB_esecuzione++; 
-        sem_post(&g->procAoB_s);
-    } else {
-        sem_post(&g->mutex);
+
+int main(int argc, char* argv[])
+{
+    srand(time(NULL));
+
+    const int k_num_threads = 10;
+    pthread_t my_threads[k_num_threads];
+
+    handler_init(&g_handler);
+
+    // stores thread ids in an array for debugging
+    int thread_ids[k_num_threads];
+    for (int i = 0; i < k_num_threads; i++)
+        thread_ids[i] = i;
+
+    // creates and starts threads
+    for (int i = 0; i < k_num_threads; i++)
+    {
+        if (pthread_create(&my_threads[i], NULL, thread_body, (void*) &thread_ids[i]) != 0)
+        {
+            perror("pthread_create() error\n");
+            return 1;
+        }
     }
-}
 
-void pausetta(void) {
-    struct timespec t;
-    t.tv_sec = 0;
-    t.tv_nsec = (rand()%10+1)*1000000;
-    nanosleep(&t, NULL);
-}
-
-void ProcA(void) {
-    printf("Eseguo A-%d\n", id_A);
-}
-
-void ProcB(void) {
-    printf("Eseguo B-%d\n", id_B);
-}
-
-void Reset(void) {
-    printf("Eseguo R-%d\n", id_R);
-}
-
-void *PA(void *arg) {
-    pthread_mutex_lock(&id_mutex);
-    id_A++;
-    int local_id = id_A;  // Copia locale per evitare race condition
-    pthread_mutex_unlock(&id_mutex);
-
-    printf("Inizio A-%d\n", local_id);
-    StartProcAorProcB(&gestore);
-    ProcA();
-    EndProcAorProcB(&gestore);
-    printf("Fine A-%d\n", local_id);
-    return 0;
-}
-
-void *PB(void *arg) {
-    pthread_mutex_lock(&id_mutex);
-    id_B++;
-    int local_id = id_B;
-    pthread_mutex_unlock(&id_mutex);
-
-    printf("Inizio B-%d\n", local_id);
-    StartProcAorProcB(&gestore);
-    ProcB();
-    EndProcAorProcB(&gestore);
-    printf("Fine B-%d\n", local_id);
-    return 0;
-}
-
-void *PR(void *arg) {
-    pthread_mutex_lock(&id_mutex);
-    id_R++;
-    int local_id = id_R;
-    pthread_mutex_unlock(&id_mutex);
-
-    printf("Inizio R-%d\n", local_id);
-    StartReset(&gestore);
-    Reset();
-    EndReset(&gestore);
-    printf("Fine R-%d\n", local_id);
-    pausetta();
-    return 0;
-}
-
-/* ------------------------------------------------------------------------ */
-
-int main(int argc, char **argv) {
-    pthread_t threads[3];
-    
-    /* Inizializzo il sistema */
-    gestore_init(&gestore);
-
-    /* Inizializzo i numeri casuali, usati nella funzione pausetta */
-    srand(555);
-
-    /* Creo i thread */
-    pthread_create(&threads[0], NULL, PA, NULL);
-    pthread_create(&threads[1], NULL, PB, NULL);
-    pthread_create(&threads[2], NULL, PR, NULL);
-
-    /* Attendo la terminazione dei thread */
-    for (int i = 0; i < 3; i++) {
-        pthread_join(threads[i], NULL);
-    }
+    sleep(60);
 
     return 0;
 }
