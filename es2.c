5f5d43ae271fda70c908411c435eb4951aa5f5fe
Ilaria00 <82587000+Ilaria00@users.noreply.github.com>
2024-09-12 17:08:53 +0200

    Update es2.c

--- es2.c
+++ es2.c
@@ -33,26 +33,29 @@ void StartProcAorProcB (struct gestore_t *g){
 
     sem_wait(&g->mutex);
 
-    //se ci sono processi reset in esecuzione mi blocco
-    if (g->reset_esecuzione) {
+    //se ci sono processi reset o AoB in esecuzione mi blocco
+    if (g->reset_esecuzione || g->AoB_esecuzione) {
         g->AoB_attesa++;
         sem_post(&g->mutex);
         sem_wait(&g->procAoB_s);
         g->AoB_attesa--;
     }
-
-    //altrimenti eseguo
-    g->AoB_esecuzione++;
+    else {
+        g->AoB_esecuzione++;
+    }
     sem_post(&g->mutex);
 }
 
 void EndProcAorProcB (struct gestore_t *g) {
 
     sem_wait(&g->mutex);
-
+    g->AoB_esecuzione--;
     //se ci sono processi reset in attesa li sveglio
     if (g->reset_attesa) {
         sem_post(&g->reset_s);
+    }
+    else if (g->AoB_attesa) {
+        sem_post(&g->procAoB_s);
     }
     else {
         sem_post(&g->mutex);
@@ -65,21 +68,23 @@ void StartReset (struct gestore_t *g) {
     sem_wait(&g->mutex);
 
     //se ci sono processi AoB in esecuzione in questo momento mi blocco
-    if (g->AoB_esecuzione) {
+    if (g->AoB_esecuzione || g->reset_esecuzione) {
         g->reset_attesa++;
         sem_post(&g->mutex);
         sem_wait(&g->reset_s);
         g->reset_attesa--;
     }
-
-    //altrimenti eseguo
-    g->reset_esecuzione++;
+    else {
+        g->reset_esecuzione++;
+    }
     sem_post(&g->mutex);
 }
 
 void EndReset (struct gestore_t *g) {
 
     sem_wait(&g->mutex);
+
+    g->reset_esecuzione--;
 
     if (g->reset_attesa) {
         sem_post(&g->reset_s);
@@ -92,80 +97,111 @@ void EndReset (struct gestore_t *g) {
     }
 }
 
-void ProcA(int thread_idx)
+void pausetta(void)
 {
-    StartProcAorProcB(&gestore);
-
-    printf("< %d-A \n", thread_idx); 
-    do_something();
-    printf("A-%d > \n", thread_idx);
-
-    EndProcAorProcB(&gestore);
+  struct timespec t;
+  t.tv_sec = 0;
+  t.tv_nsec = (rand()%10+1)*1000000;
+  nanosleep(&t,NULL);
 }
 
-void ProcB(int thread_idx)
+/* ------------------------------------------------------------------------ */
+
+/* le funzioni della risorsa R fittizia */
+
+#define BUSY 1000000
+#define CYCLE 50
+
+void myprint(char *s)
 {
-    StartProcAorProcB(&gestore);
-
-    printf("< %d-B \n", thread_idx); 
-    do_something();
-    printf("B-%d \n>", thread_idx);
-
-    EndProcAorProcB(&gestore);
+  int i,j;
+  fprintf(stderr,"[");
+  for (j=0; j<CYCLE; j++) {
+    fprintf(stderr,s);
+    for (i=0; i<BUSY; i++);
+  }
+  fprintf(stderr,"]");
+}
+void ProcA(void)
+{
+  myprint("-");
 }
 
-void Reset(int thread_idx)
+void ProcB(void)
 {
-    StartReset(&gestore);
-
-    printf("< %d-R \n", thread_idx); 
-    do_something();
-    printf("R-%d \n>", thread_idx);
-
-    EndReset(&gestore);
+  myprint("+");
 }
 
-void* thread_body(void* arg)
+void Reset(void)
 {
-    int thread_idx = *((int*) arg);
-    while (1)
-    {
-        int r = rand();
-
-        //my_printf("|%d->%s|", thread_idx, (r%3 ==0  ? "A" : (r%3==1 ? "B" : "R")));
-
-        if (r % 3 == 0) ProcA(thread_idx);
-        else if (r % 3 == 1) ProcB(thread_idx);
-        else if (r % 3 == 2) Reset(thread_idx);
-    }
+  myprint(".");
 }
 
 
-int main(int argc, char* argv[])
+void *PA(void *arg)
 {
-    srand(time(NULL));
-
-    const int k_num_threads = 10;
-    pthread_t my_threads[k_num_threads];
-
-    gestore_init(&gestore);
-
-    // stores thread ids in an array for debugging
-    int thread_ids[k_num_threads];
-    for (int i = 0; i < k_num_threads; i++)
-        thread_ids[i] = i;
-
-    // creates and starts threads
-    for (int i = 0; i < k_num_threads; i++)
-    {
-        if (pthread_create(&my_threads[i], NULL, thread_body, (void*) &thread_ids[i]) != 0)
-        {
-            perror("pthread_create() error\n");
-            return 1;
-        }
-    }
-
-    sleep(60);
-
-    return 0;
+  for (;;) {
+    fprintf(stderr,"A");
+    StartProcA();
+    ProcA();
+    EndProcA();
+    fprintf(stderr,"a");
+  }
+  return 0;
 }
+
+void *PB(void *arg)
+{
+  for (;;) {
+    fprintf(stderr,"B");
+    StartProcB();
+    ProcB();
+    EndProcB();
+    fprintf(stderr,"b");
+  }
+  return 0;
+}
+
+void *PR(void *arg)
+{
+  for (;;) {
+    fprintf(stderr,"R");
+    StartReset();
+    Reset();
+    EndReset();
+    fprintf(stderr,"r");
+    pausetta();
+  }
+  return 0;
+}
+
+/* ------------------------------------------------------------------------ */
+
+int main(int argc, char **argv)
+{
+  pthread_attr_t a;
+  pthread_t p;
+  
+  /* inizializzo il sistema */
+  myInit();
+
+  /* inizializzo i numeri casuali, usati nella funzione pausetta */
+  srand(555);
+
+  pthread_attr_init(&a);
+
+  /* non ho voglia di scrivere 10000 volte join! */
+  pthread_attr_setdetachstate(&a, PTHREAD_CREATE_DETACHED);
+
+  pthread_create(&p, &a, PA, NULL);
+  pthread_create(&p, &a, PB, NULL);
+  pthread_create(&p, &a, PR, NULL);
+
+  pthread_attr_destroy(&a);
+
+  /* aspetto 10 secondi prima di terminare tutti quanti */
+  sleep(5);
+
+  return 0;
+}
+
